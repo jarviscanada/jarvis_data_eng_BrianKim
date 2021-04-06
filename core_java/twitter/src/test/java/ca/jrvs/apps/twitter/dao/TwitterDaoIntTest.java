ed874279e98d7ce715b1aeeb6de20e17aea3626e
Brian Kim <kim.brian94@gmail.com>
2021-04-06 18:27:52 -0400

    implemented integration/unit test for TwitterDao

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
@@ -3,10 +3,11 @@ package ca.jrvs.apps.twitter.dao;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.util.TweetUtil;
-import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
-public class TwitterDaoIntTest extends TestCase {
+public class TwitterDaoIntTest {
 
   TwitterDao twitterDao;
   String mention = "TorontoStar";
@@ -26,45 +27,48 @@ public class TwitterDaoIntTest extends TestCase {
     twitterDao = new TwitterDao(httpHelper);
   }
 
+  @Test
   public void testCreate() {
     Tweet sentTweet = TweetUtil.builder(text, latitude, longitude);
     Tweet responseTweet = twitterDao.create(sentTweet);
 
-    assertEquals(text, responseTweet.getText());
+    Assert.assertEquals(text, responseTweet.getText());
 
-    assertEquals(2, responseTweet.getCoordinates().getCoordinates().length);
-    assertEquals(longitude, responseTweet.getCoordinates().getCoordinates()[0]);
-    assertEquals(latitude, responseTweet.getCoordinates().getCoordinates()[1]);
+    Assert.assertEquals(2, responseTweet.getCoordinates().getCoordinates().length);
+    Assert.assertEquals(longitude, responseTweet.getCoordinates().getCoordinates()[0]);
+    Assert.assertEquals(latitude, responseTweet.getCoordinates().getCoordinates()[1]);
 
-    assertEquals(mention, responseTweet.getEntities().getUserMentions()[0].getScreenName());
-    assertEquals(hashTag, responseTweet.getEntities().getHashtags()[0].getText());
+    Assert.assertEquals(mention, responseTweet.getEntities().getUserMentions()[0].getScreenName());
+    Assert.assertEquals(hashTag, responseTweet.getEntities().getHashtags()[0].getText());
   }
 
+  @Test
   public void testFindById() {
-    String id = "1379472129847128065";
+    String id = "1379471678179270659";
     Tweet responseTweet = twitterDao.findById(id);
 
-    assertEquals(text, responseTweet.getText());
+    Assert.assertEquals(text, responseTweet.getText());
 
-    assertEquals(2, responseTweet.getCoordinates().getCoordinates().length);
-    assertEquals(longitude, responseTweet.getCoordinates().getCoordinates()[0]);
-    assertEquals(latitude, responseTweet.getCoordinates().getCoordinates()[1]);
+    Assert.assertEquals(2, responseTweet.getCoordinates().getCoordinates().length);
+    Assert.assertEquals(longitude, responseTweet.getCoordinates().getCoordinates()[0]);
+    Assert.assertEquals(latitude, responseTweet.getCoordinates().getCoordinates()[1]);
 
-    assertEquals(mention, responseTweet.getEntities().getUserMentions()[0].getScreenName());
-    assertEquals(hashTag, responseTweet.getEntities().getHashtags()[0].getText());
+    Assert.assertEquals(mention, responseTweet.getEntities().getUserMentions()[0].getScreenName());
+    Assert.assertEquals(hashTag, responseTweet.getEntities().getHashtags()[0].getText());
   }
 
+  @Test
   public void testDeleteById() {
     String id = "1379472129847128065";
     Tweet deletedTweet = twitterDao.deleteById(id);
 
-    assertEquals(text, deletedTweet.getText());
+    Assert.assertEquals(text, deletedTweet.getText());
 
-    assertEquals(2, deletedTweet.getCoordinates().getCoordinates().length);
-    assertEquals(longitude, deletedTweet.getCoordinates().getCoordinates()[0]);
-    assertEquals(latitude, deletedTweet.getCoordinates().getCoordinates()[1]);
+    Assert.assertEquals(2, deletedTweet.getCoordinates().getCoordinates().length);
+    Assert.assertEquals(longitude, deletedTweet.getCoordinates().getCoordinates()[0]);
+    Assert.assertEquals(latitude, deletedTweet.getCoordinates().getCoordinates()[1]);
 
-    assertEquals(mention, deletedTweet.getEntities().getUserMentions()[0].getScreenName());
-    assertEquals(hashTag, deletedTweet.getEntities().getHashtags()[0].getText());
+    Assert.assertEquals(mention, deletedTweet.getEntities().getUserMentions()[0].getScreenName());
+    Assert.assertEquals(hashTag, deletedTweet.getEntities().getHashtags()[0].getText());
   }
 }
--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterHttpHelperTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterHttpHelperTest.java
@@ -2,11 +2,12 @@ package ca.jrvs.apps.twitter.dao;
 
 import com.google.gdata.util.common.base.PercentEscaper;
 import java.net.URI;
-import junit.framework.TestCase;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class TwitterHttpHelperTest extends TestCase {
+public class TwitterHttpHelperTest {
 
   TwitterHttpHelper helper = new TwitterHttpHelper(
       System.getenv("consumerKey"), System.getenv("consumerSecret"),
@@ -18,18 +19,20 @@ public class TwitterHttpHelperTest extends TestCase {
   String showUri = "https://api.twitter.com/1.1/statuses/show.json?id=1379095346366844939";
   String deleteUri = "https://api.twitter.com/1.1/statuses/destroy/1377650230611615748.json";
 
+  @Test
   public void testHttpPost() throws Exception {
     HttpResponse postResponse = helper.httpPost(URI.create(postUri));
     HttpResponse deleteResponse = helper.httpGet(URI.create(deleteUri));
 
-    assertNotNull(postResponse);
+    Assert.assertNotNull(postResponse);
     System.out.println(EntityUtils.toString(postResponse.getEntity()));
   }
 
+  @Test
   public void testHttpGet() throws Exception {
     HttpResponse showResponse = helper.httpGet(URI.create(showUri));
 
-    assertNotNull(showResponse);
+    Assert.assertNotNull(showResponse);
     System.out.println(EntityUtils.toString(showResponse.getEntity()));
   }
 }
