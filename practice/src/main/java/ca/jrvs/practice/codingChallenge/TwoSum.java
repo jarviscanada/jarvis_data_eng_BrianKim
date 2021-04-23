f3c66f7f49860834bcedbf3ef480cfa09c756633
Brian Kim <kim.brian94@gmail.com>
2021-04-22 20:01:43 -0400

    solved TwoSum and ValidAnagram

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSum.java
@@ -0,0 +1,49 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+  public static void main(String[] args) {
+    TwoSum solution = new TwoSum();
+    int[] output = solution.twoSumMap(new int[]{2, 7, 11, 15}, 18);
+    System.out.println(Arrays.toString(output));
+  }
+
+  /**
+   * Big O: O(n^2)
+   * Because of nested for loop. It will compare each number against every number in brute force way.
+   */
+  public int[] twoSumBrute(int[] nums, int target) {
+    int[] output = new int[2];
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i; j < nums.length; j++) {
+        if (i != j && nums[i] + nums[j] == target) {
+          output[0] = i;
+          output[1] = j;
+          return output;
+        }
+      }
+    }
+    return output;
+  }
+
+  /**
+   * Big O: O(n)
+   * Because of the additional Map space, the search time is reduced to O(1).
+   */
+  public int[] twoSumMap(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+      int remainder = target - nums[i];
+      if (map.containsKey(remainder) && map.get(remainder) != i)
+        return new int[]{i, map.get(remainder)};
+    }
+    throw new IllegalArgumentException("No solution");
+  }
+}
---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/ValidAnagram.java
@@ -0,0 +1,45 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+  public static void main(String[] args) {
+    ValidAnagram solution = new ValidAnagram();
+    System.out.println(solution.isAnagramTwo("listen", "silent"));
+  }
+
+  /**
+   * Big O: O(nlogn)
+   * Because for the two arrays, they are sorted and compared.
+   */
+  public boolean isAnagramOne(String s, String t) {
+    char[] sArr = s.toCharArray();
+    char[] tArr = t.toCharArray();
+    Arrays.sort(sArr);
+    Arrays.sort(tArr);
+    return Arrays.equals(sArr, tArr);
+  }
+
+  /**
+   * Big O: O(n)
+   * Because it is only iterated by length of s. And counter table is of constant size
+   */
+  public boolean isAnagramTwo(String s, String t) {
+    if (s.length() != t.length())
+      return false;
+    int[] counter = new int[26];
+
+    for (int i = 0; i < s.length(); i++) {
+      counter[s.charAt(i) - 'a']++;
+      counter[t.charAt(i) - 'a']--;
+    }
+    for (int count : counter) {
+      if (count != 0)
+        return false;
+    }
+    return true;
+  }
+}
