b32cd88e16622393bb558b4bd9c0ac4fd1b49662
Brian Kim <kim.brian94@gmail.com>
2021-04-27 20:36:34 -0400

    solved StackUsingQueue challenge

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/StackUsingOneQueue.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/StackUsingOneQueue.java
@@ -3,6 +3,9 @@ package ca.jrvs.practice.codingChallenge;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * ticket: https://www.notion.so/jarvisdev/Implement-Stack-using-Queue-834bb4f3892447b38e12df46a4a906aa
+ */
 public class StackUsingOneQueue<T> {
 
   public static void main(String[] args) {
@@ -28,6 +31,9 @@ public class StackUsingOneQueue<T> {
     queue = new LinkedList<>();
   }
 
+  /**
+   * Big O: O(n)
+   */
   public void push(T element) {
     queue.add(element);
     for (int i = 0; i < queue.size() - 1; i++)
@@ -35,14 +41,23 @@ public class StackUsingOneQueue<T> {
     top = element;
   }
 
+  /**
+   * Big O: O(1)
+   */
   public T pop() {
     return queue.remove();
   }
 
+  /**
+   * Big O: O(1)
+   */
   public T top() {
     return top;
   }
 
+  /**
+   * Big O: O(1)
+   */
   public boolean empty() {
     return queue.size() == 0;
   }
--- practice/src/main/java/ca/jrvs/practice/codingChallenge/StackUsingTwoQueue.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/StackUsingTwoQueue.java
@@ -3,6 +3,9 @@ package ca.jrvs.practice.codingChallenge;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * https://www.notion.so/jarvisdev/Implement-Stack-using-Queue-834bb4f3892447b38e12df46a4a906aa
+ */
 public class StackUsingTwoQueue<T> {
   private Queue<T> q1;
   private Queue<T> q2;
@@ -28,6 +31,10 @@ public class StackUsingTwoQueue<T> {
     q2 = new LinkedList<>();
     bool = true;
   }
+
+  /**
+   * Big O: O(1)
+   */
   public void push(T element) {
     if (bool)
       q1.add(element);
@@ -35,10 +42,16 @@ public class StackUsingTwoQueue<T> {
       q2.add(element);
   }
 
+  /**
+   * Big O: O(n)
+   */
   public T pop() {
     return removeLast();
   }
 
+  /**
+   * Big O: O(n)
+   */
   public T top() {
     T element = removeLast();
 
@@ -50,6 +63,9 @@ public class StackUsingTwoQueue<T> {
     return element;
   }
 
+  /**
+   * Big O: O(n)
+   */
   private T removeLast() {
     T element = null;
     if (bool) {
@@ -71,6 +87,9 @@ public class StackUsingTwoQueue<T> {
     return element;
   }
 
+  /**
+   * Big O: O(1)
+   */
   public boolean empty() {
     if (bool)
       return q1.size() == 0;
